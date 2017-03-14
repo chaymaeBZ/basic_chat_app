d9702d731b4874067098a6abd1afb16c4a1087f5
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-03-14 15:01:38 +0000

    more enhancements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
 User.create!(username: "alice", password: "wonderland")
 User.create!(username: "bob",   password: "azerty")
+
+50.times do |i|
+  User.create!(username: "user#{i}", password: "azerty")
+end
+ 
